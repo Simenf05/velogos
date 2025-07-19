c8f98f9c5f143cd1cb9033e07bbcfe504d98bf7d
Simen Fritsvold <sifra002@gmail.com>
2025-07-19 13:02:36 +0200

    made table for stats

--- src/statistics.rs
+++ src/statistics.rs
@@ -19,18 +19,14 @@ fn write_json_to_file(json_to_write: JsonValue) -> Result<(), io::Error> {
 
 
 fn get_empty_json() -> JsonValue {
-
     let mut json = object! {};
     let alphabet = "abcdefghijklmnopqrstuvwxyz";
-
     for letter in alphabet.chars() {
         let inner_json = object! {
-            accuracy: null,
-            wpm: null,
+            attempts: [],
         };
         json[letter.to_string()] = inner_json;
     }
-
     json
 }
 
@@ -48,7 +44,17 @@ fn get_stats_path() -> PathBuf {
         }
 
         let file_path = dir_path.join("letter_data.json");
-        let _ = File::create_new(&file_path);
+        let new_file = File::create_new(&file_path);
+
+        if new_file.is_ok() {
+            let empty_json = get_empty_json();
+            let res = write_json_to_file(empty_json);
+
+            if res.is_err() {
+                println!("Could not write to file.");
+                std::process::exit(1);
+            }
+        }
         
         return file_path;
 
@@ -59,33 +65,66 @@ fn get_stats_path() -> PathBuf {
     }
 }
 
-fn get_json_from_file() -> Result<String, io::Error> {
+fn read_file_as_string() -> Result<String, io::Error> {
     let path = get_stats_path();
     let content = read_to_string(path)?;
     Ok(content)
 }
+fn get_json_from_file() -> Result<JsonValue, io::Error> {
+    let string_content = read_file_as_string()?;
+    let content = json::parse(&string_content).expect("Failed to parse json.");
+    Ok(content)
+}
 
 
-pub fn update_stats() {
+pub fn _update_stats() {
     println!("To be implemented...");
-    let _ = write_json_to_file(object! {});
+    let _ = write_json_to_file(get_empty_json());
 }
 
 
 pub fn show_stats() -> Result<(), io::Error> {
-
-    let path = get_stats_path();
     let content = get_json_from_file()?;
 
-    let json = get_empty_json();
+    let mut letter_line = String::from("           ");
+    let mut first_line = String::from("accuracy %:  ");
+    let mut second_line = String::from("wpm:         ");
 
-    println!("{:?}", json);
+    for entry in content.entries() {
+        let letter = entry.0;
+        letter_line.push_str("      ");
+        letter_line.push_str(letter);
 
-    println!("{}", json::stringify(json));
+        let attempts = &entry.1["attempts"];
+        let attempts_count = attempts.len();
 
+        if attempts_count == 0 {
+            first_line.push_str("    N/A");
+            second_line.push_str("    N/A");
+            continue;
+        }
 
-    println!("{}", path.display());
-    println!("{}", content);
-    
+        let last_attempt = &attempts[attempts_count - 1];
+
+        let accuracy_opt = &last_attempt["acc"].as_f32();
+        if accuracy_opt.is_none() {
+            first_line.push_str("    N/A");
+        } else {
+            let accuracy = accuracy_opt.unwrap();
+            first_line = format!("{first_line}{accuracy:5}");
+        }
+
+        let wpm_opt = &last_attempt["wpm"].as_f32();
+        if wpm_opt.is_none() {
+            second_line.push_str("    N/A");
+        } else {
+            let wpm = wpm_opt.unwrap();
+            second_line = format!("{second_line}{wpm:5}");
+        }
+    }
+
+    println!("{}", letter_line);
+    println!("{}", first_line);
+    println!("{}", second_line);
     Ok(())
 }
